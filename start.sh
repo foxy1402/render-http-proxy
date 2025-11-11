807622a964d892949bfddb30a1255fc52ac0323d
foxy1402 <45601526+foxy1402@users.noreply.github.com>
2025-11-11 14:43:36 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 set -eu
 
+echo "Starting proxy in debug mode..."
+set -x
+
 PORT=${PORT:-3128}
 PROXY_USER=${PROXY_USER:-user}
 PROXY_PASS=${PROXY_PASS:-pass}
 
-# Debug: print variable values
-echo "PORT=$PORT"
-echo "PROXY_USER=$PROXY_USER"
-echo "PROXY_PASS=$PROXY_PASS"
-
-# Debug: check template and binary
 ls -l /etc/3proxy/
 cat /etc/3proxy/3proxy.cfg.tpl
 which 3proxy
