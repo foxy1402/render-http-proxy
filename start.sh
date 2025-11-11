2d7ffcfefce8e928d3c289cc0171e60f2ab8d758
foxy1402 <45601526+foxy1402@users.noreply.github.com>
2025-11-11 14:32:51 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,18 @@ PORT=${PORT:-3128}
 PROXY_USER=${PROXY_USER:-user}
 PROXY_PASS=${PROXY_PASS:-pass}
 
-sed "s/{{PORT}}/${PORT}/g; s/{{USER}}/${PROXY_USER}/g; s/{{PASS}}/${PROXY_PASS}/g"     /etc/3proxy/3proxy.cfg.tpl > /etc/3proxy/3proxy.cfg
+# Debug: print variable values
+echo "PORT=$PORT"
+echo "PROXY_USER=$PROXY_USER"
+echo "PROXY_PASS=$PROXY_PASS"
+
+# Debug: check template and binary
+ls -l /etc/3proxy/
+cat /etc/3proxy/3proxy.cfg.tpl
+which 3proxy
+
+sed "s/{{PORT}}/${PORT}/g; s/{{USER}}/${PROXY_USER}/g; s/{{PASS}}/${PROXY_PASS}/g" \
+    /etc/3proxy/3proxy.cfg.tpl > /etc/3proxy/3proxy.cfg
 
 echo "Starting 3proxy on port ${PORT} with user ${PROXY_USER}"
 exec 3proxy /etc/3proxy/3proxy.cfg
